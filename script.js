9391618cc0955b7799f2103eab861325f31abd79
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-07-02 18:20:02 -0400

    Adds background to game

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ birdCanvasRatio = 18;
 birdSpriteCanvasWidth = birdSpriteWidth / birdCanvasRatio;
 birdSpriteCanvasHeight = birdSpriteHeight / birdCanvasRatio;
 
+// background image
+const background = new Image();
+background.src = 'assets/Background.png';
+let background1X = 0;
+let background2X = CANVAS_WIDTH;
+
+
 // player object
 const player = {
     x: 20,
@@ -34,7 +41,11 @@ function animate(){
     // Declare x and y on the image for which frame of the birds animation we are currently in (this allows us to select the bird color from the sprite sheet)
     birdFrameX = (player.bird_state + player.character_choice * 8) % 4;
     birdFrameY = Math.floor((player.bird_state + player.character_choice * 8) / 4);
-    
+
+    // draw background
+    ctx.drawImage(background, background1X, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(background, background2X, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
     // draw bird
     ctx.drawImage(birdImage, birdFrameX * birdSpriteWidth + 350, birdFrameY * birdSpriteHeight + 340, birdSpriteWidth, birdSpriteHeight, player.x, player.y, birdSpriteCanvasWidth, birdSpriteCanvasHeight);
     
@@ -42,6 +53,20 @@ function animate(){
     requestAnimationFrame(animate);
 
 };
+
+// animate background moving
+setInterval(() => {
+    background1X -= LEFT_MOVEMENT_SPEED;
+    background2X -= LEFT_MOVEMENT_SPEED;
+
+    if (background1X <= -CANVAS_WIDTH) {
+        background1X = CANVAS_WIDTH;
+    }
+
+    if (background2X <= -CANVAS_WIDTH) {
+        background2X = CANVAS_WIDTH;
+    }
+}, 10);
 
 // animate bird flapping by changing bird state/frame when animation sequence is triggered (bird state is set to 1)
 function animateBird(){

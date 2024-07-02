43051d2be301c9a4eea93838cfc6cf37a7a48936
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-07-02 18:20:54 -0400

    Adds pipe logic to game

--- script.js
+++ script.js
@@ -21,6 +21,24 @@ background.src = 'assets/Background.png';
 let background1X = 0;
 let background2X = CANVAS_WIDTH;
 
+// decrare pipes image
+const pipesImage = new Image();
+pipesImage.src = 'assets/Pipes.png';
+const PIPES_SPRITE_WIDTH = 32;
+const PIPES_SPRITE_HEIGHT = 48;
+
+// pipes size on the canvas
+const PIPES_CANVAS_RATIO = 1;
+const PIPES_SPRITE_CANVAS_WIDTH = PIPES_SPRITE_WIDTH / PIPES_CANVAS_RATIO;
+const PIPES_SPRITE_CANVAS_HEIGHT = PIPES_SPRITE_HEIGHT / PIPES_CANVAS_RATIO;
+const PIPE_HOLE_WIDTH = 200;
+const PIPE_OPENING_WiDTH = 17;
+const PIPE_ROD_HEIGHT = 400;
+const PIPE_MOUTH_START_FROM_CENTER = PIPE_HOLE_WIDTH/2;
+const PIPE_ROD_START_FROM_CENTER = PIPE_HOLE_WIDTH/2 + PIPES_SPRITE_HEIGHT - PIPE_OPENING_WiDTH + PIPE_ROD_HEIGHT;
+
+// animation constants
+const LEFT_MOVEMENT_SPEED = 1;
 
 // player object
 const player = {
@@ -31,6 +49,34 @@ const player = {
     jump_speed: 3,
     character_choice: 1,
 };
+
+// TODO: create a function to randomly generate pipes, currently done manually
+pipes = [{
+    x: 100,
+    y: 300,
+    color: 1,
+}];
+
+
+function drawPipes(){
+    pipes.forEach(pipe => {
+        // Decalre x and y on the image (this allows us to select the pipe color from the sprite sheet)
+        let pipeImgX = PIPES_SPRITE_WIDTH * (pipe.color % 4);
+        let pipeImgY = PIPES_SPRITE_HEIGHT * Math.floor(pipe.color / 4);
+        
+        // Draw top of pipe mouth
+        ctx.drawImage(pipesImage, pipeImgX, pipeImgY + PIPE_OPENING_WiDTH, PIPES_SPRITE_WIDTH, PIPES_SPRITE_HEIGHT - PIPE_OPENING_WiDTH, pipe.x , pipe.y- PIPE_MOUTH_START_FROM_CENTER, PIPES_SPRITE_CANVAS_WIDTH, PIPES_SPRITE_CANVAS_HEIGHT);
+
+        // Draw bottom pipe mouth
+        ctx.drawImage(pipesImage, pipeImgX, pipeImgY, PIPES_SPRITE_WIDTH, PIPES_SPRITE_HEIGHT - PIPE_OPENING_WiDTH, pipe.x, pipe.y + PIPE_MOUTH_START_FROM_CENTER, PIPES_SPRITE_CANVAS_WIDTH, PIPES_SPRITE_CANVAS_HEIGHT);
+
+        // Draw full pipe top and bottom
+        ctx.drawImage(pipesImage, pipeImgX, pipeImgY + PIPE_OPENING_WiDTH, PIPES_SPRITE_WIDTH, PIPES_SPRITE_HEIGHT - PIPE_OPENING_WiDTH*2, pipe.x, pipe.y - PIPE_HOLE_WIDTH/2 - PIPES_SPRITE_HEIGHT + PIPE_OPENING_WiDTH - PIPE_ROD_HEIGHT + PIPES_SPRITE_CANVAS_HEIGHT, PIPES_SPRITE_CANVAS_WIDTH, PIPE_ROD_HEIGHT);
+        ctx.drawImage(pipesImage, pipeImgX, pipeImgY + PIPE_OPENING_WiDTH, PIPES_SPRITE_WIDTH, PIPES_SPRITE_HEIGHT - PIPE_OPENING_WiDTH*2, pipe.x, pipe.y + PIPE_HOLE_WIDTH/2 + PIPES_SPRITE_CANVAS_HEIGHT, PIPES_SPRITE_CANVAS_WIDTH, PIPE_ROD_HEIGHT); 
+
+    }   
+    );  
+}
 
 // main loop to animate the game
 function animate(){
@@ -45,6 +91,9 @@ function animate(){
     // draw background
     ctx.drawImage(background, background1X, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(background, background2X, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    // draw pipes
+    drawPipes();
 
     // draw bird
     ctx.drawImage(birdImage, birdFrameX * birdSpriteWidth + 350, birdFrameY * birdSpriteHeight + 340, birdSpriteWidth, birdSpriteHeight, player.x, player.y, birdSpriteCanvasWidth, birdSpriteCanvasHeight);
